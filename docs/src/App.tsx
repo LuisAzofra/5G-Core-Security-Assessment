1e000b4bd2ade89ea8b87d21e7b676fe30fa1ab0
Luis Azofra Begara <luis.azofra@cabify.com>
2025-08-20 23:01:50 +0200

    chore: trigger pages redeploy (comment)

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,3 +1,4 @@
+// minor whitespace change to trigger GH Pages redeploy
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
